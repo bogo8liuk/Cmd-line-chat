2d51286f50926673ac217a8601317a9e60753d85
bogo8liuk <lucaborghi99@gmail.com>
2020-12-25 00:18:29 +0100

    cl_init

--- cl_utils.h
+++ cl_utils.h
@@ -2,8 +2,20 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <inttypes.h>
+#include <sys/socket.h>
+
+#define NO_PROTOCOL	0
 
 #define bad_exit(...)				fprintf(stderr, __VA_ARGS__);	\
 									exit(EXIT_FAILURE)
 
-extern bool port_conversion(char *port, uint16_t *res);
+struct cl_init_struct {
+	struct sockaddr_in entry;
+	int domain;
+	int type;
+	int protocol;
+};
+
+extern struct cl_init_struct *cl_init(char *const address, char *const port, int domain, int type, int protocol);
+
+extern bool port_conversion(char *const port, uint16_t *res);
